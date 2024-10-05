c6c77d57d01ef71eff910d69d65a57a3e5ccfd52
AhmedSamir <ahmed.samirelbakrey@gmail.com>
2024-10-05 03:59:21 +0300

    remote access

--- db/queries.js
+++ db/queries.js
@@ -2,5 +2,5 @@
  * hna b2a bytkteb kol el queries bta3t el app kolo 
  */
 exports.queryList = {
-    GET_STORE_LIST_QUERY : 'SELECT * FROM  university.student' ,
+    GET_STORE_LIST_QUERY : 'SELECT * FROM public.student' ,
 }
